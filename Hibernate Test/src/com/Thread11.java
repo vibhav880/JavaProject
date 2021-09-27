e54b4dbce3889d3d4c57bffa9d265a0f5d8cdef9
vibhav880 <74583476+vibhav880@users.noreply.github.com>
2021-09-27 23:16:09 +0530

    Adding some text in threadd 111

--- Hibernate Test/src/com/Thread11.java
+++ Hibernate Test/src/com/Thread11.java
@@ -4,7 +4,8 @@ public class Thread11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("added some text in thread11");
+
+		System.out.println("Enter the text in threDAD11");
 	}
 
 }
